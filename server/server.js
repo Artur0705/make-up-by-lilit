a8b9fa7ac2948602f18d04e21b119b020e152fe3
Artur0705 <artlil420@gmail.com>
2023-10-07 01:17:35 +1100

    Initialized folder structure

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
