0bf275271665f573bf8c03801d156ead11939e32
Artur0705 <artlil420@gmail.com>
2023-10-07 01:52:24 +1100

    Created database connection and set up the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const db = require("./config/connection.js");
+
 require("dotenv").config();
 
 const app = express();
@@ -9,3 +11,14 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Hello from server");
+  res.send("Test from server");
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on PORT:${PORT}`);
+  });
+});
