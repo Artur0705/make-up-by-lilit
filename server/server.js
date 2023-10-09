68de62f826588e30503e271fcfb44b508dcdb069
Artur0705 <artlil420@gmail.com>
2023-10-09 20:43:12 +1100

    Added `morgan` package to track the requests easily

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const db = require("./config/connection.js");
 
@@ -8,6 +9,8 @@ require("dotenv").config();
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(cors());
