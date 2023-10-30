915217fed2062a91c21740616348dbcb9faede67
Artur0705 <artlil420@gmail.com>
2023-10-30 21:45:46 +1100

    Added sharp package to reduce the page load time by compressing the images

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,23 +1,32 @@
 const cloudinary = require("../utils/cloudinaryConfig");
-// const { verifyToken } = require("../middlewares/authMiddleware");
+const sharp = require("sharp");
 
 exports.uploadImage = [
-  //   verifyToken,
-  (req, res) => {
-    console.log(req.headers); // Check the request headers
-    console.log(req.body); // Check the request body
+  async (req, res) => {
+    try {
+      // Resize and compress image using sharp
+      const optimizedBuffer = await sharp(req.file.buffer)
+        .resize({ width: 800 }) // Resize to 800px width
+        .jpeg({ quality: 80 }) // Compress to 80% quality
+        .toBuffer();
 
-    const fileStr = req.file.buffer.toString("base64");
+      // Convert buffer to base64
+      const fileStr = optimizedBuffer.toString("base64");
 
-    cloudinary.uploader.upload(
-      `data:${req.file.mimetype};base64,${fileStr}`,
-      (error, result) => {
-        if (error) {
-          return res.status(500).send(error);
+      // Upload optimized image to Cloudinary
+      cloudinary.uploader.upload(
+        `data:${req.file.mimetype};base64,${fileStr}`,
+        (error, result) => {
+          if (error) {
+            return res.status(500).send(error);
+          }
+
+          res.json({ imageUrl: result.secure_url });
         }
-
-        res.json({ imageUrl: result.secure_url });
-      }
-    );
+      );
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error.message);
+    }
   },
 ];
