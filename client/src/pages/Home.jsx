62f2ef0db4d18dff6030e31a2ec43284e4846673
Artur0705 <artlil420@gmail.com>
2023-10-30 20:23:21 +1100

    Added a button in home page to navigate to services page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Welcome from "../components/Welcome";
 import EventsIntro from "../components/EventsIntro";
 import Fashion from "../components/Fashion";
 import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -13,6 +14,11 @@ export default function Home() {
         <Welcome />
         <EventsIntro />
         <Fashion />
+        <div className="text-white text-center text-xl italic">
+          <Link to="/services">
+            <button className="btn text-center">More Services</button>
+          </Link>
+        </div>
       </Layout>
     </>
   );
